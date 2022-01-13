f91d4fa7028357475960e30c2ff3cb3d0dbbbbaf
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-01-13 17:12:44 -0500

    Theme Dark Complete
    It's complete with CSS and JS, a of hour the do click, change the theme dark

--- SRC/JS/features.js
+++ SRC/JS/features.js
@@ -1,6 +1,7 @@
 // Declaracion de Variables
 
 const icon = document.getElementById("icon");
+const main = document.getElementById("search");
 
 //*Funcion de los demas resultados
 
@@ -18,12 +19,43 @@ icon.addEventListener("click", () => {
 //*Funcion para cambiar el color de fondo
 
 function changeLight() {
+  // Call Body
   const body = document.getElementById("per");
+  // If  the class than contents body is bady
   if ( body.classList.contains("bady")) {
     body.classList.replace("bady", "light");
+    // Change the icon
+    changeIcon();
+    // Change main
+    changeMainSearch();
   } else {
+    reverIcon();
     body.classList.replace("light", "bady");
     body.classList.add("efect");
+    reverseMainSearch();
     
   }
+}
+
+// Function the icon
+
+const changeIcon = () => {
+  icon.classList.replace("bi-cloud-sun", "bi-cloud-sun-fill");
+  icon.style.color = "black";
+}
+
+const reverIcon = () => {
+    icon.classList.replace("bi-cloud-sun-fill", "bi-cloud-sun");
+    icon.classList.add("full");
+    icon.style.color = "white";
+}
+
+// Function the main
+
+function changeMainSearch() {
+  main.style.background = "#190474ed";
+}
+
+function reverseMainSearch() {
+  main.style.background = "rgba( 255, 255, 255, 0.25 )";
 }
