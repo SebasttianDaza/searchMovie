b8b20bb14390843b577907275a6fd71d8f8f0532
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-01-12 21:00:53 -0500

    Add effect theme ligth
    I was working in the effect with JS and CSS

--- SRC/JS/features.js
+++ SRC/JS/features.js
@@ -18,6 +18,12 @@ icon.addEventListener("click", () => {
 //*Funcion para cambiar el color de fondo
 
 function changeLight() {
-  const body = document.getElementById("bady");
+  const body = document.getElementById("per");
+  if ( body.classList.contains("bady")) {
+    body.classList.replace("bady", "light");
+  } else {
+    body.classList.replace("light", "bady");
+    body.classList.add("efect");
+    
+  }
 }
-
